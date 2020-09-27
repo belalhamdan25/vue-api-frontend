d8ba95a8876802ae304b69ada880a067b4257d83
belalhamdan25 <belal.hamdan25@gmail.com>
2020-09-27 03:26:24 +0300

    Skeleton Card Loding

--- src/views/Portfolios.vue
+++ src/views/Portfolios.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row py-4">
         <div class="col-md-3 col-sm-12 w-100 text-center">
-        <button  style="background-color:#00B3EC;" type="button" class="btn btn-primary mb-4 form-control">Primary</button>
+        <button  type="button" class="btn btn-primary mb-4 form-control">Primary</button>
         <div class="active-cyan-4 mb-4">
         <input class="form-control" type="text" placeholder="Filter" aria-label="Filter">
       </div>
@@ -60,18 +60,28 @@
               <div class="card portfolio-card border-0  p-2">
                 <div class=" d-flex justify-content-start align-items-center">
                   <div class="d-flex align-items-center px-1 w-img-100">
-                    <img
-                      class=" hw-50 rounded-circle"
-                      :src="Portfolio.user_img"
-                      alt="user image"
-                    />
+
+                  <img
+                  v-if="loading"
+                  class=" hw-50 rounded-circle"
+                  :src="Portfolio.user_img"
+                  alt="user image"
+                />
+                <div class="hw-50 rounded-circle" style="background-color:#E6E6E6" v-else>
+                  
+                </div>
+
                   </div>
                   <div class="pl-2">
                     <div class=" justify-content-center pr-1 align-content-end">
-                      <h6 class="card-name m-0 h-fit">
+                      <h6  v-if="loading" class="card-name m-0 h-fit">
                         {{ Portfolio.user_name }}
                       </h6>
-                      <small class="text-muted">{{ Portfolio.title }}</small>
+                      <div class="card-name mb-2 h-fit" v-else style="width:50px;height:5px;background-color:#E6E6E6"></div>
+
+                      <small v-if="loading" class="text-muted">{{ Portfolio.title.substring(0,20)+".." }}</small>
+                      <div class="text-muted" v-else style="width:100px;height:5px;background-color:#E6E6E6"></div>
+
                     </div>
                   </div>
                 </div>
@@ -79,7 +89,10 @@
                   class="card-img-top img-raduis-bottom"
                   :src="Portfolio.img"
                   :alt="Portfolio.desc"
+                  v-if="loading"
                 />
+                
+                <img  v-else src="https://e.top4top.io/p_17305yynx1.png" class="card-img-top img-raduis-bottom">
                 <!-- <div class="card-body text-center">
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div> -->
@@ -126,6 +139,7 @@
   margin-bottom: 0 !important ;
 }
 
+
 /* .img-raduis-bottom{
       border-radius: 0px 0px 10px 10px;
 } */
@@ -135,9 +149,11 @@
 import axios from "axios";
 
 export default {
+
   data() {
     return {
       Portfolios: {},
+      loading: false
     };
   },
   methods: {
@@ -157,20 +173,30 @@ export default {
         });
 
         this.scrollTop();
+        this.doStuff();
     },
     scrollTop: function() {
       this.intervalId = setInterval(() => {
         if (window.pageYOffset === 0) {
           clearInterval(this.intervalId);
         }
-        window.scroll(0, window.pageYOffset - 50);
+        window.scroll(0, window.pageYOffset - 100);
       }, 20);
     },
+    doStuff: function() {
+        //Show Loader
+        this.loading = false;
 
+        //Waste 5 seconds
+        setTimeout(() => {
+           this.loading = true;
+        }, 1000)
+    }
   },
   mounted() {
     this.loadPortfolios();
     this.getResults();
+    this.doStuff();
 
   },
 
