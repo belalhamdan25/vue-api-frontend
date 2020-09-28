62236f317c39c50f4558b128848129c4b3f2d25c
belalhamdan25 <belal.hamdan25@gmail.com>
2020-09-28 04:17:10 +0300

    portfolio search

--- src/views/Portfolios.vue
+++ src/views/Portfolios.vue
@@ -3,121 +3,265 @@
     <div class="container">
       <div class="row py-4">
         <div class="col-md-3 col-sm-12 w-100 text-center">
-        <button  type="button" class="btn btn-primary mb-4 form-control">Primary</button>
-        <div class="active-cyan-4 mb-4">
-        <input class="form-control" type="text" placeholder="Filter" aria-label="Filter">
-      </div>
-              <div class="active-cyan-4 mb-4">
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-      </div>
+          <button type="button" class="btn btn-primary mb-4 form-control">
+            Primary
+          </button>
+          <div class="active-cyan-4 mb-4">
+            <input
+              class="form-control"
+              type="text"
+              placeholder="Filter"
+              aria-label="Filter"
+            />
+          </div>
+          <div class="active-cyan-4 mb-4">
+            <input
+              class="form-control"
+              type="text"
+              placeholder="Search"
+              v-model="query"
+              aria-label="Search"
+            />
+            <button
+              class="btn btn-default"
+              type="button"
+              @click="search()"
+              v-if="!searchLoading"
+            >
+              Search!
+            </button>
+            <button
+              class="btn btn-default"
+              type="button"
+              disabled="disabled"
+              v-if="searchLoading"
+            >
+              Searching...
+            </button>
+            <div class="alert alert-danger" role="alert" v-if="error">
+              <span
+                class="glyphicon glyphicon-exclamation-sign"
+                aria-hidden="true"
+              ></span>
+              {{ error }}
+            </div>
+          </div>
 
-      <div class="text-left mb-4">
-        <h5>Categories</h5>
-                <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="">Option 1
-    </label>
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="">Option 2
-    </label>
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="" >Option 3
-    </label>
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="">Option 1
-    </label>
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="">Option 2
-    </label>
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input" value="">Option 3
-    </label>
-  </div>
-      </div>
+          <div class="text-left mb-4">
+            <h5>Categories</h5>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 1
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 2
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 3
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 1
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 2
+              </label>
+            </div>
+            <div class="form-check">
+              <label class="form-check-label">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  value=""
+                />Option 3
+              </label>
+            </div>
+          </div>
 
-                    <div class="active-cyan-4 mb-4">
-        <input class="form-control" type="text" placeholder="Skills" aria-label="Skills">
-      </div>
-
+          <div class="active-cyan-4 mb-4">
+            <input
+              class="form-control"
+              type="text"
+              placeholder="Skills"
+              aria-label="Skills"
+            />
+          </div>
         </div>
         <div class="col-md-9  col-sm-12">
-          <div class="row">
-            <div
-              class="col-lg-4 col-md-6 col-sm-6 pb-4  d-flex justify-content-center align-content-center"
-              v-for="Portfolio in Portfolios.data"
-              :key="Portfolio.id"
-            >
-              <div class="card portfolio-card border-0  p-2">
-                <div class=" d-flex justify-content-start align-items-center">
-                  <div class="d-flex align-items-center px-1 w-img-100">
+          <div v-if="searchPortfolios.length">
+            <div class="row">
+              <div
+                class="col-lg-4 col-md-6 col-sm-6 pb-4  d-flex justify-content-center align-content-center"
+                v-for="searchPortfolio in searchPortfolios"
+                :key="searchPortfolio.id"
+              >
+                <div class="card portfolio-card border-0  p-2">
+                  <div class=" d-flex justify-content-start align-items-center">
+                    <div class="d-flex align-items-center px-1 w-img-100">
+                      <img
+                        v-if="loading"
+                        class=" hw-50 rounded-circle"
+                        :src="searchPortfolio.user_img"
+                        alt="user image"
+                      />
+                      <div
+                        class="hw-50 rounded-circle"
+                        style="background-color:#E6E6E6"
+                        v-else
+                      ></div>
+                    </div>
+                    <div class="pl-2">
+                      <div
+                        class=" justify-content-center pr-1 align-content-end"
+                      >
+                        <h6 v-if="loading" class="card-name m-0 h-fit">
+                          {{ searchPortfolio.user_name }}
+                        </h6>
+                        <div
+                          class="card-name mb-2 h-fit"
+                          v-else
+                          style="width:50px;height:5px;background-color:#E6E6E6"
+                        ></div>
 
-                  <img
-                  v-if="loading"
-                  class=" hw-50 rounded-circle"
-                  :src="Portfolio.user_img"
-                  alt="user image"
-                />
-                <div class="hw-50 rounded-circle" style="background-color:#E6E6E6" v-else>
-                  
-                </div>
-
-                  </div>
-                  <div class="pl-2">
-                    <div class=" justify-content-center pr-1 align-content-end">
-                      <h6  v-if="loading" class="card-name m-0 h-fit">
-                        {{ Portfolio.user_name }}
-                      </h6>
-                      <div class="card-name mb-2 h-fit" v-else style="width:50px;height:5px;background-color:#E6E6E6"></div>
-
-                      <small v-if="loading" class="text-muted">{{ Portfolio.title.substring(0,20)+".." }}</small>
-                      <div class="text-muted" v-else style="width:100px;height:5px;background-color:#E6E6E6"></div>
-
+                        <small v-if="loading" class="text-muted">{{
+                          searchPortfolio.title.substring(0, 20) + ".."
+                        }}</small>
+                        <div
+                          class="text-muted"
+                          v-else
+                          style="width:100px;height:5px;background-color:#E6E6E6"
+                        ></div>
+                      </div>
                     </div>
                   </div>
+                  <img
+                    class="card-img-top img-raduis-bottom"
+                    :src="searchPortfolio.img"
+                    :alt="searchPortfolio.desc"
+                    v-if="loading"
+                  />
+
+                  <img
+                    v-else
+                    src="https://e.top4top.io/p_17305yynx1.png"
+                    class="card-img-top img-raduis-bottom"
+                  />
+                  <!-- <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div> -->
                 </div>
-                <img
-                  class="card-img-top img-raduis-bottom"
-                  :src="Portfolio.img"
-                  :alt="Portfolio.desc"
-                  v-if="loading"
-                />
-                
-                <img  v-else src="https://e.top4top.io/p_17305yynx1.png" class="card-img-top img-raduis-bottom">
-                <!-- <div class="card-body text-center">
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div> -->
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-12 pl-3">
-              <pagination
-                :data="Portfolios"
-                @pagination-change-page="getResults"
-                class="margin-b-0 border-0"
+
+          <div v-else>
+            <div class="row">
+              <div
+                class="col-lg-4 col-md-6 col-sm-6 pb-4  d-flex justify-content-center align-content-center"
+                v-for="Portfolio in Portfolios.data"
+                :key="Portfolio.id"
               >
-                <span slot="prev-nav">Previous</span>
-                <span slot="next-nav">Next</span>
-              </pagination>
+                <div class="card portfolio-card border-0  p-2">
+                  <div class=" d-flex justify-content-start align-items-center">
+                    <div class="d-flex align-items-center px-1 w-img-100">
+                      <img
+                        v-if="loading"
+                        class=" hw-50 rounded-circle"
+                        :src="Portfolio.user_img"
+                        alt="user image"
+                      />
+                      <div
+                        class="hw-50 rounded-circle"
+                        style="background-color:#E6E6E6"
+                        v-else
+                      ></div>
+                    </div>
+                    <div class="pl-2">
+                      <div
+                        class=" justify-content-center pr-1 align-content-end"
+                      >
+                        <h6 v-if="loading" class="card-name m-0 h-fit">
+                          {{ Portfolio.user_name }}
+                        </h6>
+                        <div
+                          class="card-name mb-2 h-fit"
+                          v-else
+                          style="width:50px;height:5px;background-color:#E6E6E6"
+                        ></div>
+
+                        <small v-if="loading" class="text-muted">{{
+                          Portfolio.title.substring(0, 20) + ".."
+                        }}</small>
+                        <div
+                          class="text-muted"
+                          v-else
+                          style="width:100px;height:5px;background-color:#E6E6E6"
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                  <img
+                    class="card-img-top img-raduis-bottom"
+                    :src="Portfolio.img"
+                    :alt="Portfolio.desc"
+                    v-if="loading"
+                  />
+
+                  <img
+                    v-else
+                    src="https://e.top4top.io/p_17305yynx1.png"
+                    class="card-img-top img-raduis-bottom"
+                  />
+                  <!-- <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 pl-3">
+                <pagination
+                  :data="Portfolios"
+                  @pagination-change-page="getResults"
+                  class="margin-b-0 border-0"
+                >
+                  <span slot="prev-nav">Previous</span>
+                  <span slot="next-nav">Next</span>
+                </pagination>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-            
-
   </section>
-  
 </template>
 
 <style scoped>
@@ -139,7 +283,6 @@
   margin-bottom: 0 !important ;
 }
 
-
 /* .img-raduis-bottom{
       border-radius: 0px 0px 10px 10px;
 } */
@@ -149,11 +292,14 @@
 import axios from "axios";
 
 export default {
-
   data() {
     return {
       Portfolios: {},
-      loading: false
+      loading: false,
+      error: false,
+      query: "",
+      searchLoading: false,
+      searchPortfolios: [],
     };
   },
   methods: {
@@ -172,8 +318,8 @@ export default {
           this.Portfolios = response.data;
         });
 
-        this.scrollTop();
-        this.doStuff();
+      this.scrollTop();
+      this.doStuff();
     },
     scrollTop: function() {
       this.intervalId = setInterval(() => {
@@ -184,22 +330,52 @@ export default {
       }, 20);
     },
     doStuff: function() {
-        //Show Loader
-        this.loading = false;
+      //Show Loader
+      this.loading = false;
 
-        //Waste 5 seconds
-        setTimeout(() => {
-           this.loading = true;
-        }, 1000)
-    }
+      //Waste 5 seconds
+      setTimeout(() => {
+        this.loading = true;
+      }, 1000);
+    },
+    search: function() {
+      if(this.query == ""){
+       this.searchPortfolios = [];
+            this.doStuff();
+
+      }else{
+                // Clear the error message.
+        this.error = "";
+        // Empty the products array so we can fill it with the new products.
+        this.searchPortfolios = [];
+        // Set the loading property to true, this will display the "Searching..." button.
+        this.searchLoading = true;
+        this.loading = false;
+        // Making a get request to our API and passing the query to it.
+        axios
+          .get(
+            "http://localhost/vue-api-backend/public/api/portfolio/portfolios/search?q=" +
+              this.query
+          )
+          .then((response) => {
+            // If there was an error set the error message, if not fill the products array.
+            response.data.error
+              ? (this.error = response.data.error)
+              : (this.searchPortfolios = response.data);
+            // The request is finished, change the loading to false again.
+            this.loading = true;
+            this.searchLoading = false;
+            // Clear the query.
+            this.query = "";
+          });
+      }
+    },
   },
   mounted() {
     this.loadPortfolios();
     this.getResults();
     this.doStuff();
-
   },
-
 };
 </script>
 portfolio
