17c5c5e98a34d88b16303043ab46ad4ac1b09bdb
belalhamdan25 <belal.hamdan25@gmail.com>
2020-11-08 03:51:51 +0200

    search box home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,259 +1,267 @@
 <template>
   <div class="home">
     <div class="slide-back-image">
-  <div class="overlay-effect"></div>
-    <div class="container">
-      <div class="row">
+      <div class="overlay-effect"></div>
+      <div class="container">
+        <div class="row">
           <div class="center">
-        <div class="col-12 ">
-          <h2 class="text-white font-weight-bold">
-            Hire the best freelancers for any job, online.
-          </h2>
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Your Project Title" />
-            <div class="input-group-append">
-              <button class="btn btn-success" type="submit">Go</button>
-            </div>
-          </div>
-        </div>
-          </div>
-      </div>
-    </div>
-  </div>
-     <section class="how-works bg-white text-center">
-    <div class="container">
-      <header class="section-header">
-        <h3>How Worker Works</h3>
-      </header>
-      <div class="row row-eq-height justify-content-center">
-        <div class="col-lg-4 mb-4">
-          <div class="">
-            <i class="fas fa-file-invoice font-9 "></i>
-            <div class="card-body">
-              <h5 class="card-title">Add Project</h5>
-              <p class="card-text">
-                Add project for free and be ready to choose the best deal
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-hand-holding-usd font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Choose Deal</h5>
-              <p class="card-text">
-                Choose the best deal and start the project proccess
-              </p>
-            </div>
-          </div>
-        </div>
+            <div class="col-12 ">
+              <div
+                class="d-flex  justify-content-center flex-column align-items-center"
+              >
+                <h1 class="text-white text-center font-weight-bold">
+                  Hire the best freelancers <br />
+                  for any job, online.
+                </h1>
 
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-user-tie font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Hire The Best</h5>
-              <p class="card-text">Choose the best worker and hire him</p>
+
+                  <div class="input-group search-input mt-3">
+                <input type="text" class="form-control p-4" placeholder="Hire Freelancer" />
+                <div class="input-group-append">
+                  <!-- <button class="btn btn-success" type="submit">Go</button> -->
+                  <vs-button
+                  class="btn btn-success p-2 m-0 background-startsec1"
+                  color="#41b883"
+                  flat
+                    icon
+                    animation-type="rotate"
+                  >
+                    <i style="color:white" class='bx bx-search'></i>
+                    <template #animate >
+                      <i style="color:white" class='bx bx-search-alt' ></i>
+                    </template>
+                  </vs-button>
+                </div>
+              </div>
+              </div>
+
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
-     <section class="why-us  text-center">
-    <div class="container">
-      <header class="section-header">
-        <h3>Why Worker ?</h3>
-      </header>
-      <div class="row row-eq-height justify-content-center">
-
-        <div class="col-lg-4 mb-4">
-          <div class="">
-            <i class="fas fa-briefcase font-9 "></i>
-            <div class="card-body">
-              <h5 class="card-title">Compleate Project
- easily</h5>
-              <p class="card-text">
-                Add your project and leave the task 
-of implementing it to the 
-best experts in the  world
-              </p>
+    <section class="how-works bg-white text-center py-5">
+      <div class="container">
+        <header class="section-header mb-5">
+          <h3>How Worker Works</h3>
+        </header>
+        <div class="row row-eq-height justify-content-center">
+          <div class="col-lg-4 mb-4">
+            <div class="">
+              <i class="fas fa-file-invoice font-9 "></i>
+              <div class="card-body">
+                <h5 class="card-title">Add Project</h5>
+                <p class="card-text">
+                  Add project for free and be ready to choose the best deal
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-paper-plane font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Hire The Best
-Freelancer</h5>
-              <p class="card-text">
-                Choose the freelancers you see
-and hire them directly
-              </p>
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-hand-holding-usd font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Choose Deal</h5>
+                <p class="card-text">
+                  Choose the best deal and start the project proccess
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-credit-card font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Pay with your
- rights guaranteed</h5>
-              <p class="card-text">pay for the work that is done
-and the balance remains in your account
- until the project complete</p>
-            </div>
-          </div>
-        </div>
-
-
-        
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-users font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Biuld Amizang 
-teams</h5>
-              <p class="card-text">
-                Experts in various fields 
-and from different 
-countries are ready to serve you
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class=" ">
-            <i class="fas fa-user-tie font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Get your business
- done quickly</h5>
-              <p class="card-text">Browse the freelancers , see their 
-work spamples, and skills
-and hire the best</p>
-            </div>
-          </div>
-        </div>
-
-                <div class="col-lg-4 mb-4">
-          <div class="">
-            <i class="fas fa-dollar-sign font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">your projects
- at the lowest cost</h5>
-              <p class="card-text">
-                Hire the best expertise 
-according to your budget
-              </p>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </section>
- <div class="categories bg-white text-center">
-    <div class="container">
-      <header class="section-header">
-        <h3>Categories</h3>
-      </header>
-      <div class="row row-eq-height justify-content-center">
-        <div class="col-lg-4 mb-4">
-          <div class="card">
-            <i class="fas fa-paint-brush font-9 "></i>
-            <div class="card-body">
-              <h5 class="card-title">Design and works</h5>
-              <p class="card-text">
-                Graphic design interior engineering and planning Motion graphic
-                design
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class=" card">
-            <i class="fas fa-language font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Translation and languages</h5>
-              <p class="card-text">
-                Legal translation, general translation, and technical
-                translation
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class="card ">
-            <i class="fas fa-code font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Programming and development</h5>
-              <p class="card-text">
-                Software development, programming, support and application
-                development
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class="card ">
-            <i class="fas fa-pencil-alt font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Writing and editing</h5>
-              <p class="card-text">
-                Content writing, research and reports, editing and auditing
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class="card">
-            <i class="fas fa-lightbulb font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Sales and marketing</h5>
-              <p class="card-text">
-                E-marketing, social media campaigns, and pricing strategies
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-4 mb-4">
-          <div class="card ">
-            <i class="fas fa-comments font-9"></i>
-            <div class="card-body">
-              <h5 class="card-title">Consulting and training</h5>
-              <p class="card-text">
-                Provide advice, experiences and reviews and share experiences
-              </p>
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-user-tie font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Hire The Best</h5>
+                <p class="card-text">Choose the best worker and hire him</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </section>
+    <section class="why-us py-5  text-center">
+      <div class="container">
+        <header class="section-header mb-5">
+          <h3>Why Worker ?</h3>
+        </header>
+        <div class="row row-eq-height justify-content-center">
+          <div class="col-lg-4 mb-4">
+            <div class="">
+              <i class="fas fa-briefcase font-9 "></i>
+              <div class="card-body ">
+                <h5 class="card-title">Compleate Project easily</h5>
+                <p class="card-text">
+                  Add your project and leave the task of implementing it to the
+                  best experts in the world
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-paper-plane font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Hire The Best Freelancer</h5>
+                <p class="card-text">
+                  Choose the freelancers you see and hire them directly
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-credit-card font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Pay with your rights guaranteed</h5>
+                <p class="card-text">
+                  pay for the work that is done and the balance remains in your
+                  account until the project complete
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-users font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Biuld Amizang teams</h5>
+                <p class="card-text">
+                  Experts in various fields and from different countries are
+                  ready to serve you
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class=" ">
+              <i class="fas fa-user-tie font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">Get your business done quickly</h5>
+                <p class="card-text">
+                  Browse the freelancers , see their work spamples, and skills
+                  and hire the best
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="">
+              <i class="fas fa-dollar-sign font-9"></i>
+              <div class="card-body">
+                <h5 class="card-title">your projects at the lowest cost</h5>
+                <p class="card-text">
+                  Hire the best expertise according to your budget
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="categories bg-white text-center py-5">
+      <div class="container">
+        <header class="section-header mb-5">
+          <h3>Categories</h3>
+        </header>
+        <div class="row row-eq-height justify-content-center">
+          <div class="col-lg-4 mb-4">
+            <div class="card p-4">
+              <i class="fas fa-paint-brush font-9 "></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Design and works</h5>
+                <p class="card-text">
+                  Graphic design interior engineering and planning Motion
+                  graphic design
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class=" card p-4">
+              <i class="fas fa-language font-9"></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Translation and languages</h5>
+                <p class="card-text">
+                  Legal translation, general translation, and technical
+                  translation
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="card p-4 ">
+              <i class="fas fa-code font-9"></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Programming and development</h5>
+                <p class="card-text">
+                  Software development, programming, support and application
+                  development
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="card p-4">
+              <i class="fas fa-pencil-alt font-9"></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Writing and editing</h5>
+                <p class="card-text">
+                  Content writing, research and reports, editing and auditing
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="card p-4">
+              <i class="fas fa-lightbulb font-9"></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Sales and marketing</h5>
+                <p class="card-text">
+                  E-marketing, social media campaigns, and pricing strategies
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="card p-4">
+              <i class="fas fa-comments font-9"></i>
+              <div class="card-body d-flex  justify-content-center flex-column align-items-center">
+                <h5 class="card-title">Consulting and training</h5>
+                <p class="card-text">
+                  Provide advice, experiences and reviews and share experiences
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div> 
-      <div class="background-startsec ">
-    <div class="container ">
-      <div class="row ">
+    <div class="background-startsec ">
+      <div class="container ">
+        <div class="row ">
           <div class="col-12 text-center">
-           <div class="p-5">
-            <h1 class="text-white ">
-              Are You Ready To Start Your Project?
-            </h1>
-            <button>Start Now</button>
+            <div class="p-5">
+              <h1 class="text-white ">
+                Are You Ready To Start Your Project?
+              </h1>
+              <button>Start Now</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -261,12 +269,12 @@ according to your budget
 .slide-back-image {
   background-image: url("./../../src/assets/images/homepage-slide-8.jpg");
   height: 60vh; /* You must set a specified height */
-    /* Create the parallax scrolling effect */
+  /* Create the parallax scrolling effect */
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-    position: relative;
+  position: relative;
 }
 .center {
   margin: 0;
@@ -275,8 +283,9 @@ according to your budget
   left: 50%;
   -ms-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
+  width: 100%;
 }
-.overlay-effect{
+.overlay-effect {
   position: absolute;
   top: 0;
   bottom: 0;
@@ -284,19 +293,30 @@ according to your budget
   right: 0;
   opacity: 1;
   background-color: #0000002f;
-  overflow:hidden;
+  overflow: hidden;
 }
 .font-9 {
   font-size: 50px;
 }
 .background-startsec {
-  background-color: #41B883;
+  background-color: #41b883;
 }
+.search-input{
+  width: 50%;
+}
+
+.background-startsec1 {
+  background-color: #41b883;
+}
+.background-startsec1:hover {
+  background-color: #369a6e;
+}
+.card{
+  min-height: 280px;
+}
+
 </style>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
-
