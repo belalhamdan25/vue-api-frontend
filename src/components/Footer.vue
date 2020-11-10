dc49370f03f7c32c53133cc01c7c331bcc4be6cc
belalhamdan25 <belal.hamdan25@gmail.com>
2020-11-11 00:04:42 +0200

    footer update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
         <!-- Grid column -->
         <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
           <!-- Content -->
-          <h6 class="text-uppercase font-weight-bold">Worker.com</h6>
+          <h6 class="text-uppercase font-weight-bold text-dark">Worker</h6>
           <hr
             class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto"
             style="width: 60px;"
@@ -34,7 +34,7 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Terms</h6>
+          <h6 class="text-uppercase font-weight-bold text-dark">Terms</h6>
           <hr
             class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto"
             style="width: 60px;"
@@ -51,7 +51,7 @@
           <p>
             <a class="dark-grey-text" href="#!">Code of Conduct</a>
           </p>
-                    <p>
+          <p>
             <a class="dark-grey-text" href="#!">Fees and Charges</a>
           </p>
         </div>
@@ -60,13 +60,16 @@
         <!-- Grid column -->
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Links</h6>
+          <h6 class="text-uppercase font-weight-bold text-dark">Links</h6>
           <hr
             class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto"
             style="width: 60px;"
           />
           <p>
-            <a class="dark-grey-text" href="#!">Work Saples</a>
+            <a class="dark-grey-text" href="#!">Portfolios</a>
+          </p>
+          <p>
+            <a class="dark-grey-text" href="#!">Projects</a>
           </p>
           <p>
             <a class="dark-grey-text" href="#!">Freelancers</a>
@@ -80,15 +83,44 @@
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <h6 class="text-uppercase font-weight-bold text-dark">Follow Us</h6>
           <hr
             class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto"
             style="width: 60px;"
           />
-          <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-          <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-          <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+          <div
+            class="  onmobilealigin"
+          >
+            <div class="d-flex  justify-content-center  align-items-center">
+              <vs-button icon color="facebook">
+                <i class="bx bxl-facebook-square"></i>
+              </vs-button>
+              <vs-button icon color="twitter">
+                <i class="bx bxl-twitter"></i>
+              </vs-button>
+            </div>
+
+          </div>
+
+                    <h6 class="text-uppercase font-weight-bold text-dark mt-4">Payments</h6>
+          <hr
+            class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto"
+            style="width: 60px;"
+          />
+
+          <div
+            class=" onmobilealigin"
+          >
+            <div class="d-flex  justify-content-center  align-items-center">
+              <i class="fab fa-cc-visa mr-2" style="font-size:30px"></i>
+              <i class="fab fa-cc-paypal mr-2" style="font-size:30px"></i>
+              <i class="fab fa-cc-mastercard mr-2" style="font-size:30px"></i>
+            </div>
+
+          </div>
+
+
         </div>
         <!-- Grid column -->
       </div>
@@ -101,12 +133,41 @@
       class="footer-copyright text-center text-black-50 py-3"
       style="background-color:#E9E9E9"
     >
-      © 2020 Copyright:
-      <a class="dark-grey-text" href="#">
-        Worker.com</a
-      >
+        © 2020 Worker.com - All rights reserved 
     </div>
     <!-- Copyright -->
   </footer>
   <!-- Footer -->
 </template>
+
+<style scoped>
+.dark-grey-text {
+  color: #666;
+}
+a {
+  text-decoration: none;
+  transition: 0.3s;
+}
+a:hover {
+  color: rgb(56, 56, 56);
+  transition: 0.3s;
+}
+@media only screen and (max-width: 767px) {
+  .onmobilealigin {
+    display: flex !important;
+    flex-direction: column !important;
+    justify-content: center !important;
+    align-items: center !important;
+  }
+}
+
+@media only screen and (min-width: 767px) {
+  .onmobilealigin {
+    display: flex !important;
+    flex-direction: column !important;
+    justify-content: center !important;
+    align-items: start !important;
+  }
+
+}
+</style>
