6185cfc44bf244ab060723ce831358c6d5b5bb30
belalhamdan25 <belal.hamdan25@gmail.com>
2020-11-27 23:06:23 +0200

    user profile nav

--- src/views/UserProfileShow.vue
+++ src/views/UserProfileShow.vue
@@ -1,57 +1,55 @@
 <template>
   <section>
     <div class="container" v-for="userValue in userValues" :key="userValue.id">
-      <div class="row my-4 ">
+      <div class="row my-4">
         <div class="col-12">
           <div class="row">
             <div class="col-12 radios-5 bg-white pt-4 pr-4 pl-4">
-              <div class="d-none d-md-block"> 
-                                <div
-                class="profile-header d-flex justify-content-between align-items-center mx-4"
-              >
+              <div class="d-none d-md-block">
                 <div
-                  class="d-flex flex-column justify-content-center align-items-center"
+                  class="profile-header d-flex justify-content-between align-items-center mx-4"
                 >
-                  <img
-                    v-if="userValue.user_img != null"
-                    class="circle2"
-                    :src="userValue.user_img"
-                  />
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-center"
+                  >
+                    <img
+                      v-if="userValue.user_img != null"
+                      class="circle2"
+                      :src="userValue.user_img"
+                    />
 
-                  <div v-else class="circle">
-                    <span style="color: white; font-size: 25px">{{
-                      userValue.user_name.charAt(0).toUpperCase()
-                    }}</span>
+                    <div v-else class="circle">
+                      <span style="color: white; font-size: 25px">{{
+                        userValue.user_name.charAt(0).toUpperCase()
+                      }}</span>
+                    </div>
+
+                    <h6 class="mt-4">{{ userValue.user_name }}</h6>
                   </div>
 
-                  <h6 class="mt-4">{{ userValue.user_name }}</h6>
-                </div>
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-start"
+                    style="color: #666"
+                  >
+                    <p>
+                      <i class="fas fa-user-tag"></i> {{ userValue.category }}
+                    </p>
+                    <p>
+                      <i class="fas fa-map-marker-alt"></i>
+                      {{ userValue.location }}
+                    </p>
+                  </div>
 
-                <div
-                  class="d-flex flex-column justify-content-center align-items-start"
-                  style="color: #666"
-                >
-                  <p>
-                    <i class="fas fa-user-tag"></i> {{ userValue.category }}
-                  </p>
-                  <p>
-                    <i class="fas fa-map-marker-alt"></i>
-                    {{ userValue.location }}
-                  </p>
-                </div>
-
-                                    <div
-                  class="d-flex flex-column justify-content-center align-items-center"
-                >
-                  <div style="position: relative; width: 225px">
-                    <radar-chart :user-data="arrayuserData"></radar-chart>
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-center"
+                  >
+                    <div style="position: relative; width: 225px">
+                      <radar-chart :user-data="arrayuserData"></radar-chart>
+                    </div>
                   </div>
                 </div>
-               
-              </div>
 
-              <nav class="navbar navbar-expand-lg navbar-light bg-white p-0 mx-0">
-
+                <!-- <nav class="navbar navbar-expand-lg navbar-light bg-white p-0 mx-0">
               <div class="navbar-collapse">
                 <div class="navbar-nav">
                   <a class="nav-item nav-link active-user-nav" href="#"><i class='bx bx-user'></i> About me<span class="sr-only">(current)</span></a>
@@ -61,47 +59,108 @@
                   <a class="nav-item nav-link" href="#"><i class='bx bx-star' ></i> Rates</a>
                 </div>
               </div>
-            </nav>
+            </nav> -->
+
+                <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                  <li class="nav-item">
+                    <a
+                      class="nav-link active"
+                      id="pills-about-tab"
+                      data-toggle="pill"
+                      href="#pills-about"
+                      role="tab"
+                      aria-controls="pills-about"
+                      aria-selected="true"
+                      ><i class='bx bx-user'></i> About me</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-portfolio-tab"
+                      data-toggle="pill"
+                      href="#pills-portfolio"
+                      role="tab"
+                      aria-controls="pills-portfolio"
+                      aria-selected="false"
+                      ><i  class='bx bx-photo-album'></i> Portfolio</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-offers-tab"
+                      data-toggle="pill"
+                      href="#pills-offers"
+                      role="tab"
+                      aria-controls="pills-offers"
+                      aria-selected="false"
+                      ><i class='bx bx-spreadsheet'></i> Offers</a
+                    >
+                  </li>
+                    <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-projects-tab"
+                      data-toggle="pill"
+                      href="#pills-projects"
+                      role="tab"
+                      aria-controls="pills-projects"
+                      aria-selected="false"
+                      ><i class='bx bx-briefcase-alt'></i> Projects</a
+                    >
+                  </li>
+                   <li class="nav-item">
+                    <a
+                      class="nav-link"
+                      id="pills-rates-tab"
+                      data-toggle="pill"
+                      href="#pills-rates"
+                      role="tab"
+                      aria-controls="pills-rates"
+                      aria-selected="false"
+                      ><i class='bx bx-star' ></i> Rates</a
+                    >
+                  </li>
+                </ul>
               </div>
 
               <div class="d-block d-sm-block d-md-none">
-                                                  <div
-                class="profile-header d-flex flex-column justify-content-center align-items-center "
-              >
                 <div
-                  class="d-flex flex-column justify-content-center align-items-center"
+                  class="profile-header d-flex flex-column justify-content-center align-items-center"
                 >
-                  <img
-                    v-if="userValue.user_img != null"
-                    class="circle2"
-                    :src="userValue.user_img"
-                  />
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-center"
+                  >
+                    <img
+                      v-if="userValue.user_img != null"
+                      class="circle2"
+                      :src="userValue.user_img"
+                    />
 
-                  <div v-else class="circle">
-                    <span style="color: white; font-size: 25px">{{
-                      userValue.user_name.charAt(0).toUpperCase()
-                    }}</span>
+                    <div v-else class="circle">
+                      <span style="color: white; font-size: 25px">{{
+                        userValue.user_name.charAt(0).toUpperCase()
+                      }}</span>
+                    </div>
+
+                    <h6 class="mt-4">{{ userValue.user_name }}</h6>
                   </div>
 
-                  <h6 class="mt-4">{{ userValue.user_name }}</h6>
+                  <div
+                    class="d-flex flex-column justify-content-center align-items-center"
+                    style="color: #666"
+                  >
+                    <p>
+                      <i class="fas fa-user-tag"></i> {{ userValue.category }}
+                    </p>
+                    <p>
+                      <i class="fas fa-map-marker-alt"></i>
+                      {{ userValue.location }}
+                    </p>
+                  </div>
                 </div>
-
-                <div
-                  class="d-flex flex-column justify-content-center align-items-center"
-                  style="color: #666"
-                >
-                  <p>
-                    <i class="fas fa-user-tag"></i> {{ userValue.category }}
-                  </p>
-                  <p>
-                    <i class="fas fa-map-marker-alt"></i>
-                    {{ userValue.location }}
-                  </p>
-                </div>
-               
               </div>
-              </div>
-
             </div>
           </div>
         </div>
@@ -110,12 +169,72 @@
       <div class="row mb-4">
         <div class="col-lg-9 col-md-8 col-sm-12">
           <div class="row mr-lg-2 mr-md-2">
-            <div class="col-12 mb-2  radios-5 bg-white p-4">
-              <div class="about">
-                <h6>About Me</h6>
-                <hr />
+            <div class="col-12 mb-2 radios-5 bg-white p-4">
+              <div class="tab-content" id="pills-tabContent">
+                <div
+                  class="tab-pane fade show active"
+                  id="pills-about"
+                  role="tabpanel"
+                  aria-labelledby="pills-about-tab"
+                >
+                  <div class="about">
+                    <h6>About Me</h6>
+                    <hr />
+                    <p style="color: #666">{{ userValue.about }}</p>
+                  </div>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="pills-portfolio"
+                  role="tabpanel"
+                  aria-labelledby="pills-portfolio-tab"
+                >
+                  <div class="portfolio">
+                    <h6>Portfolio</h6>
+                    <hr />
 
-                <p style="color: #666">{{ userValue.about }}</p>
+
+
+
+                  </div>
+                </div>
+                <div
+                  class="tab-pane fade"
+                  id="pills-offers"
+                  role="tabpanel"
+                  aria-labelledby="pills-offers-tab"
+                >
+                   <div class="offers">
+                    <h6>Offers</h6>
+                    <hr />
+
+                  </div>
+                </div>
+                      <div
+                  class="tab-pane fade"
+                  id="pills-projects"
+                  role="tabpanel"
+                  aria-labelledby="pills-projects-tab"
+                >
+                   <div class="projects">
+                    <h6>Projects</h6>
+                    <hr />
+
+                  </div>
+                </div>
+
+                                      <div
+                  class="tab-pane fade"
+                  id="pills-rates"
+                  role="tabpanel"
+                  aria-labelledby="pills-rates-tab"
+                >
+                   <div class="rates">
+                    <h6>Rates</h6>
+                    <hr />
+
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -128,38 +247,38 @@
                 <hr />
                 <div class="d-flex justify-content-between align-items-center">
                   <p class="">Rate</p>
-                          <star-rating
-                          class="mb-2"
-                          :rating="userValue.rate"
-                          :border-width="2"
-                          :read-only="true"
-                          inactive-color="#fff"
-                          border-color="#ffd055"
-                          :star-size="13"
-                          :show-rating="false"
-                          :star-points="[
-                            23,
-                            2,
-                            14,
-                            17,
-                            0,
-                            19,
-                            10,
-                            34,
-                            7,
-                            50,
-                            23,
-                            43,
-                            38,
-                            50,
-                            36,
-                            34,
-                            46,
-                            19,
-                            31,
-                            17,
-                          ]"
-                        ></star-rating>
+                  <star-rating
+                    class="mb-2"
+                    :rating="userValue.rate"
+                    :border-width="2"
+                    :read-only="true"
+                    inactive-color="#fff"
+                    border-color="#ffd055"
+                    :star-size="13"
+                    :show-rating="false"
+                    :star-points="[
+                      23,
+                      2,
+                      14,
+                      17,
+                      0,
+                      19,
+                      10,
+                      34,
+                      7,
+                      50,
+                      23,
+                      43,
+                      38,
+                      50,
+                      36,
+                      34,
+                      46,
+                      19,
+                      31,
+                      17,
+                    ]"
+                  ></star-rating>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                   <p>Joind</p>
@@ -169,18 +288,17 @@
                 </div>
               </div>
             </div>
-            <div class="col-12 bg-white  p-4 radios-5">
+            <div class="col-12 bg-white p-4 radios-5">
               <div class="skills">
                 <h6>My Skills</h6>
                 <hr />
 
                 <span
-                class="label-tag"
-                    v-for="userSkills in userValue.skills"
-                              :key="userSkills.id"
-                              ><i class="fas fa-tag"></i> {{ userSkills.name }}
-                            </span>
-
+                  class="label-tag"
+                  v-for="userSkills in userValue.skills"
+                  :key="userSkills.id"
+                  ><i class="fas fa-tag"></i> {{ userSkills.name }}
+                </span>
               </div>
             </div>
           </div>
@@ -235,7 +353,7 @@ p {
   font-size: 14px;
   color: rgb(87, 87, 87);
 }
-a{
+a {
   font-size: 15px;
   color: rgb(87, 87, 87);
 }
@@ -266,18 +384,30 @@ a{
   background: #369a6e;
   transition: 0.3s;
 }
-.active-user-nav{
-border-bottom: 3px solid #41b883;
-background: #f5f4f9;
-}
-.nav-link{
+/* .active {
+  border-bottom: 3px solid #41b883;
+  background: #f5f4f9;
+} */
+.nav-link {
   padding: 12px;
-  color:#666
+  color: #666;
+
 }
-.nav-link:hover{
-background: #f5f4f9;
-color:#666
+.nav-link:hover {
+  background: #f5f4f9;
+  color: #666;
+  border-radius: 0;
+  border-bottom: 3px solid #f5f4f9;
+
+
 }
+.nav-pills .nav-link.active, .nav-pills .show > .nav-link {
+    color: #666;
+    background-color: #f5f4f9;
+    border-radius: 0;
+border-bottom: 3px solid #41b883;
+}
+
 </style>
 
 <script>
@@ -288,7 +418,7 @@ import StarRating from "vue-star-rating";
 export default {
   components: {
     RadarChart,
-    StarRating
+    StarRating,
   },
   data() {
     return {
